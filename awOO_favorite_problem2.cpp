bf1b65b68b86887ac108d5777666743647396a38
Istiaq Fuad <istiaqfuad123@gmail.com>
2022-06-13 00:52:29 +0600

    making some changes after solving problem of finished contest

---
+++ awOO_favorite_problem2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int t, n;
+  cin >> t;
+
+  while (t--) {
+    cin >> n;
+    string s, t;
+    cin >> s >> t;
+
+    for (int i = 0; i < n; i++) {
+      if (s[i] != t[i] && i < n - 1) {
+        int j = i + 1;
+        while (j < n && s[j] == s[i]) j++;
+        if (j < n && s[j] == t[i] && s[j] == s[i] + 1) swap(s[j], s[i]);
+      }
+    }
+    if (s == t)
+      cout << "YES" << '\n';
+    else
+      cout << "NO" << '\n';
+  }
+  return 0;
+}
---
+++ promo2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, q;
+  cin >> n >> q;
+
+  vector<int> a(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+
+  sort(a.begin(), a.end());
+
+  vector<long long> pref(n + 1);
+  for (int i = 0; i < n; i++) {
+    pref[i + 1] = pref[i] + a[i];
+  }
+
+  while (q--) {
+    int x, y;
+    cin >> x >> y;
+    cout << pref[n - (x - y)] - pref[n - x] << '\n';
+  }
+
+  return 0;
+}
